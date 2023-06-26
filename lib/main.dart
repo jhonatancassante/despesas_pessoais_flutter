c16c8048a9cf4a5890c161dca5e6081a3955394e
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-06-26 19:45:14 -0300

    Iniciando nova aplicação

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const DespesasPessoais());
+
+class DespesasPessoais extends StatelessWidget {
+  const DespesasPessoais({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Despesas Pessoais'),
+        ),
+        body: const Center(
+          child: Text('Versão Inicial!'),
+        ));
+  }
+}
