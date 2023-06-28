e54a89de7733846a6ddea7f12a11f659f6682953
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-06-28 10:23:39 -0300

    Criado componente Grafico

---
+++ lib/components/grafico.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:despesas_pessoal/models/transacao.dart';
+
+class Grafico extends StatelessWidget {
+  final List<Transacao> transacoesRecentes;
+
+  const Grafico(this.transacoesRecentes, {super.key});
+
+  List<Map<String, Object>> get agruparTransacoes {
+    return List.generate(7, (index) {
+      final diaSemana = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double somaTotal = 0.0;
+
+      for (var i = 0; i < transacoesRecentes.length; i++) {
+        bool mesmoDia = transacoesRecentes[i].data.day == diaSemana.day;
+        bool mesmoMes = transacoesRecentes[i].data.month == diaSemana.month;
+        bool mesmoAno = transacoesRecentes[i].data.year == diaSemana.year;
+
+        if (mesmoDia && mesmoMes && mesmoAno) {
+          somaTotal += transacoesRecentes[i].valor;
+        }
+      }
+
+      print(DateFormat.E().format(diaSemana)[0]);
+      print(somaTotal);
+
+      return {
+        'dia': DateFormat.E().format(diaSemana)[0],
+        'valor': somaTotal,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
